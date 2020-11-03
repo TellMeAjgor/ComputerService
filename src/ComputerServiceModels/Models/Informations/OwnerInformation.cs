ba5ffcc84ae99d724a8a258fd2a0f349f04d23d2
TellMeAjgor <igorjedrzejek@gmail.com>
2020-11-04 00:17:48 +0100

    Added Owner

---
+++ src/ComputerServiceModels/Models/Informations/OwnerInformation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ComputerServiceModels.Models.Informations
+{
+    public class OwnerInformation
+    {
+        public int OwnerId { get; set; }
+        public string Email { get; set; }
+        [EmailAddress]
+        public string Password { get; set; }
+        public string Name { get; set; }
+    }
+}
