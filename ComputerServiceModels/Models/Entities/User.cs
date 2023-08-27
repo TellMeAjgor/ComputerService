c286eee2acf58a61156d0ee7627cce66c7218c45
IgorJedrzejek <igorjedrzejek@gmail.com>
2023-08-28 01:59:24 +0200

    project init

---
+++ ComputerServiceModels/Models/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerServiceModels.Models.Entities
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid UserId { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
