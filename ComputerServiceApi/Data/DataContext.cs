73905af31d5a0c5addba473cc20419a60abffcda
IgorJedrzejek <igorjedrzejek@gmail.com>
2023-08-28 02:06:07 +0200

    Remove ComputerServiceApi submodule

---
+++ ComputerServiceApi/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using ComputerServiceModels.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ComputerService.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ ComputerServiceApi/MappingProfile/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ComputerServiceModels.Models.Entities;
+using ComputerServiceModels.Models.Informations;
+
+namespace ComputerService.MappingProfile
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile() 
+        {
+            CreateMap<User, UserInformation>().ReverseMap();
+        }  
+    }
+}
